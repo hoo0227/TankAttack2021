b60106852074afa9d395e9f4b3e44d200f203c3d
Hooin Shin <hoo0227@gmail.com>
2021-04-27 10:41:45 +0900

    Tank

--- Assets/02.Scripts/PhotonManager.cs
+++ Assets/02.Scripts/PhotonManager.cs
@@ -25,9 +25,42 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         base.OnConnectedToMaster();
         {
             Debug.Log("Connected to Photon Server!!!");
+            PhotonNetwork.JoinRandomRoom(); //랜덤한 룸에 접속 시도
         }
 
     }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"code={returnCode}, msg={message}");
+       
+        //룸 생성
+        PhotonNetwork.CreateRoom("My Room");
+    }
+
+    //룸 생성 완료 콜백
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("방 생성 완료");
+        
+    }
+
+    //룸에 입장했을 때 호출되는 콜백
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("방 입장 완료");
+        Debug.Log(PhotonNetwork.CurrentRoom.Name);
+    }
+
+
+
+
+
+
+
+
+
+
 
     
 }
