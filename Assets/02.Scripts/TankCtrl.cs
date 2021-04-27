75490cd48977ba8f531ba8245d06cf610ce88fa7
Hooin Shin <hoo0227@gmail.com>
2021-04-27 11:53:33 +0900

    connect other server

--- Assets/02.Scripts/TankCtrl.cs
+++ Assets/02.Scripts/TankCtrl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityStandardAssets.Utility;
 
 public class TankCtrl : MonoBehaviour
 {
@@ -13,7 +14,17 @@ public class TankCtrl : MonoBehaviour
     {
         tr = GetComponent<Transform>();
         pv = GetComponent<PhotonView>();
+
+        if (pv.IsMine)
+        {
+            Camera.main.GetComponent<SmoothFollow>().target = tr.Find("CamPivot").transform;
         GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -0.5f, 0);
+        }
+
+        else 
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 
     void Update()
